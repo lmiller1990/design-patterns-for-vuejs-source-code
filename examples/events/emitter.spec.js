bc60591262954bac94a1dfb281b462440737d653
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-11-23 17:51:04 +1000

    update examples

--- examples/events/emitter.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import Emitter from './Emitter.vue'
-
-test('app', async () => {
-  const wrapper = mount(Emitter) 
-  await wrapper.find('input').setValue('Lachlan')
-  console.log(wrapper.html())
-  console.log(wrapper.emitted())
-})
